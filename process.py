a52eb11315ebe95e74c7c6d6a92f7234094001b3
Bartłomiej Boczek <goterix1@gmail.com>
2018-06-08 00:33:32 +0200

    Processing functions made

--- process.py
+++ process.py
@@ -4,8 +4,9 @@ import os
 import re
 import pandas as pd
 
-stemmer = PorterStemmer()
-lemmatiser = WordNetLemmatizer()
+from functional import seq
+from functools import partial
+
 
 csv_file = "tweets_apple.csv"
 
@@ -13,15 +14,54 @@ os.path.isfile(csv_file)
 print("Loading from file:", csv_file, sep=" ")
 df = pd.read_csv(csv_file)
 df.reindex()
-for index, row in df.iterrows():
-    full = row["full_text"]
-    wordlist = re.sub("[^\w]", " ",  full).split()
-    #print(wordlist)
-    for word in wordlist:
-        #print(word)
-        lem = lemmatiser.lemmatize(word, pos="v")
-        stem = stemmer.stem(word)
-        d = {'index': [index], 'word': [word], 'stem' :[stem], 'lem':[lem]}
-        data = pd.DataFrame(data=d)
-        print(data)
-        #print("Index: %s, Word: %s, Stem: %s, Lemma: %s" % (index, word, stem, lem))
+
+
+def tokenize_and_remove_punkt(full_text: str) -> list:
+    """
+    Tokenize and remove punctuation
+    Parameters
+    ----------
+    full_text: str
+        text
+
+    Returns
+    -------
+        list of words
+    """
+    return re.sub("[^\w]", " ", full_text).split()
+
+
+def stem(words: list, stemmer_cls=PorterStemmer) -> list:
+    """
+    Stem every word in list
+    Parameters
+    ----------
+    words : list
+        List with words
+    stemmer_cls : class
+        stemmer class
+
+    Returns
+    -------
+        List of stemmed words
+    """
+    stemmer = stemmer_cls()
+    return seq(words).map(stemmer.stem).list()
+
+
+def lem(words: list, lemmatiser_cls=WordNetLemmatizer) -> list:
+    """
+    Apply lemming on every word in list
+    Parameters
+    ----------
+    words : list
+        List with words
+    lemmatiser_cls : class
+        lemmer class
+
+    Returns
+    -------
+        List of lemmed words
+    """
+    lemmatiser = lemmatiser_cls()
+    return seq(words).map(partial(lemmatiser.lemmatiser, pos='v')).list()
